e5be14cab010be6cb716648155343f30cbf689fd
NoahTheSej <63048083+NoahTheSej@users.noreply.github.com>
2020-04-02 12:55:05 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,21 +1,21 @@
 -- CONFIG --
-
+ 
 -- Sound volume (0.0 - 1.0)
 local volume = 1.0
-
+ 
 -- CODE --
-
-
-
+ 
+ 
+ 
 Citizen.CreateThread(function()
-	while true do
-		Wait(1)
-
-		if IsControlJustPressed(2, 19) then
-				SendNUIMessage({
-					playdeathsound = true,
-					deathvolume = volume
-				})
-			end
-		end
-	end)
+    while true do
+        Wait(1)
+ 
+        if IsControlJustPressed(2, 19) then
+                SendNUIMessage({
+                    playdeathsound = true,
+                    deathvolume = volume
+                    })
+                end
+            end
+        end)
